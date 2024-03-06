b6364d5cc4ffa3b73cfdfbbf5483499c69f826ba
alope0420 <alexander@opedal.org>
2024-03-07 00:14:47 +0100

    add server side validation

--- src/main/java/no/oslomet/alope0420/data1700/oblig2/Ticket.java
+++ src/main/java/no/oslomet/alope0420/data1700/oblig2/Ticket.java
@@ -1,11 +1,30 @@
 package no.oslomet.alope0420.data1700.oblig2;
 
+import jakarta.validation.constraints.*;
+
 public class Ticket {
+
+    @NotBlank
     public String movie;
+
+    @Min(value = 1)
+    @Max(value = 100)
     public Integer count;
+
+    @NotBlank
     public String firstname;
+
+    @NotBlank
     public String lastname;
+
+    @Pattern(regexp = "^[2-9]\\d{7}|(?:\\+|00)\\d{6,}$")
     public String tel;
+
+    /*
+    * Validation pattern for e-mail addresses, adapted for case-insensitivity. Source: How to Find or Validate an Email Address. (n.d.).
+    * Retrieved February 11, 2024, from https://www.regular-expressions.info/email.html
+    */
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$")
     public String email;
 
     public Ticket(String movie, Integer count, String firstname, String lastname, String tel, String email) {
--- src/main/java/no/oslomet/alope0420/data1700/oblig2/TicketController.java
+++ src/main/java/no/oslomet/alope0420/data1700/oblig2/TicketController.java
@@ -1,18 +1,20 @@
 package no.oslomet.alope0420.data1700.oblig2;
 
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 
+@Validated
 @RestController
 public class TicketController {
 
-    private final ArrayList<Ticket> tickets = new ArrayList<Ticket>();
+    private final ArrayList<Ticket> tickets = new ArrayList<>();
 
     @PostMapping("/tickets/add")
-    public void addTickets(Ticket ticket) {
+    public void addTickets(@Valid Ticket ticket) {
         tickets.add(ticket);
     }
 
