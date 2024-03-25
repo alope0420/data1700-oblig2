655d15d7e053b5375f497692326e9ca9b0a9581f
alope0420 <alexander@opedal.org>
2024-03-25 12:55:47 +0100

    add spinners on buttons that trigger async calls

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,26 +3,25 @@
 // Define all existing field ids, so we can loop through them instead of copy-pasting everything 6 times
 const fieldIds = ['movie', 'count', 'firstname', 'lastname', 'tel', 'email'];
 
-
 $('document').ready(() => {
     $('#dark-mode-switch').change(ev => {
-        $('html').attr('data-bs-theme', ev.target.checked ? 'dark' : null);
+        $('html').attr('data-bs-theme', ev.currentTarget.checked ? 'dark' : null);
     });
 
     $('#client-validation-switch').click(ev => {
-        $('#add-tickets-form').attr('novalidate', ev.target.checked ? null : '');
+        $('#add-tickets-form').attr('novalidate', ev.currentTarget.checked ? null : true);
     });
 
     // Add validation styles when user has entered data and unfocused input
     $('#add-tickets-form input, select')
         .blur(ev => {
-            if (ev.target.value) {
-                ev.target.parentElement.classList.add('was-validated');
+            if ($(ev.currentTarget).val()) {
+                $(ev.currentTarget).parent().addClass('was-validated');
             }
         })
         // ... or when they try to submit with invalid fields
         .on('invalid', ev => {
-            ev.target.parentElement.classList.add('was-validated');
+            $(ev.currentTarget).parent().addClass('was-validated');
         });
 
     $('#add-tickets-form').submit(ev => {
@@ -36,26 +35,48 @@ $('document').ready(() => {
 })
 
 async function addTickets() {
-    // Construct ticket object
-    let ticket = {};
-    for (const fieldId of fieldIds) {
-        ticket[fieldId] = $('#' + fieldId).val();
+    try {
+        // Disable button and show spinner
+        $('#add-tickets-button').prop('disabled', true);
+        $('#add-tickets-button i').removeClass('d-none');
+
+        // Construct ticket object
+        let ticket = {};
+        for (const fieldId of fieldIds) {
+            ticket[fieldId] = $('#' + fieldId).val();
+        }
+
+        // Clear form and reset validation
+        $('#add-tickets-form').trigger('reset');
+        $('#add-tickets-form .was-validated').removeClass('was-validated');
+
+        // Post ticket order to backend
+        await $.post('/tickets/add', ticket);
+
+        // Add ticket to tickets array and refresh table
+        await refreshTicketTable();
+
+    } finally {
+        // Hide spinner on button
+        $('#add-tickets-button').prop('disabled', false);
+        $('#add-tickets-button i').addClass('d-none');
     }
-
-    // Clear form and reset validation
-    $('#add-tickets-form').trigger('reset');
-    $('#add-tickets-form .was-validated').removeClass('was-validated');
-
-    // Post ticket order to backend
-    await $.post('/tickets/add', ticket);
-
-    // Add ticket to tickets array and refresh table
-    await refreshTicketTable();
 }
 
 async function clearTickets() {
-    await $.post('/tickets/clear');
-    await refreshTicketTable();
+    try {
+        // Disable button and show spinner
+        $('#clear-tickets-button').prop('disabled', true);
+        $('#clear-tickets-button i').removeClass('d-none');
+
+        await $.post('/tickets/clear');
+        await refreshTicketTable();
+
+    } finally {
+        // Hide spinner on button
+        $('#clear-tickets-button').prop('disabled', false);
+        $('#clear-tickets-button i').addClass('d-none');
+    }
 }
 
 async function refreshTicketTable() {
@@ -79,17 +100,33 @@ async function refreshTicketTable() {
 }
 
 async function fillDummyInfo() {
-    let dummyInfo = await $.get('https://randomuser.me/api/?nat=no');
-    dummyInfo = dummyInfo.results[0];
+    try {
+        // Disable button and change icon to spinner
+        $(this).prop('disabled', true);
+        $(this).find('.bi').addClass('d-none');
+        $(this).find('.spinner-border').removeClass('d-none');
 
-    const movieOptions = $('#movie option[value!=""]');
-    const movieIndex = Math.floor(Math.random() * movieOptions.length);
-    const ticketCount = Math.ceil(Math.min(...Array.from({length: 20}, Math.random)) * 100);
+        // Get dummy information from API, and take first result only
+        let dummyInfo = await $.get('https://randomuser.me/api/?nat=no');
+        dummyInfo = dummyInfo.results[0];
 
-    $('#movie').val(movieOptions[movieIndex].value);
-    $('#count').val(ticketCount);
-    $('#firstname').val(dummyInfo.name.first);
-    $('#lastname').val(dummyInfo.name.last);
-    $('#tel').val(dummyInfo.phone);
-    $('#email').val(dummyInfo.email);
+        // Generate random movie choice and ticket count
+        const movieOptions = $('#movie option[value!=""]');
+        const movieIndex = Math.floor(Math.random() * movieOptions.length);
+        const ticketCount = Math.ceil(Math.min(...Array.from({length: 20}, Math.random)) * 100);
+
+        // Fill form with dummy values
+        $('#movie').val(movieOptions[movieIndex].value);
+        $('#count').val(ticketCount);
+        $('#firstname').val(dummyInfo.name.first);
+        $('#lastname').val(dummyInfo.name.last);
+        $('#tel').val(dummyInfo.phone);
+        $('#email').val(dummyInfo.email);
+
+    } finally {
+        // Enable button and change icon back
+        $(this).find('.bi').removeClass('d-none');
+        $(this).find('.spinner-border').addClass('d-none');
+        $(this).prop('disabled', false);
+    }
 }
